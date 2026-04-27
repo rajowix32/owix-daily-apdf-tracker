0b8ddb724d391f56e3be95c8bb03f6d8b7db11a5
rajowix32 <rajowix32@gmail.com>
2026-04-27 06:37:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -294,11 +294,9 @@ def login_page():
     col1, col2, col3 = st.columns([1, 2, 1])
     with col2:
         st.markdown("<br><br>", unsafe_allow_html=True)
-                c1, c2 = st.columns([1, 5])
-
+        c1, c2 = st.columns([1, 5])
         with c1:
             st.image("logo.png", width=55)
-
         with c2:
             st.markdown("## OWIX Daily Tracker")
         st.markdown("##### Login to continue")
